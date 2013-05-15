7588c640bbba1c62211da79da3437fad4521ed60
Bobby <bellfalasch@gmail.com>
2013-05-15 17:20:26 +0200

    Added new (and correct) create table SQL

--- _admin/DATABASE.sql
+++ _admin/DATABASE.sql
@@ -1,49 +1,23 @@
--- phpMyAdmin SQL Dump
--- version 3.4.5
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Oct 17, 2012 at 08:38 AM
--- Server version: 5.5.16
--- PHP Version: 5.3.8
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `test`
---
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `nxtcms_users`
+-- Table structure for table `ffueater`
 --
 
-CREATE TABLE IF NOT EXISTS `nxtcms_users` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NULL,
-  `mail` varchar(255) NOT NULL,
-  `username` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  `lastlogin` datetime DEFAULT NULL,
-  `level` int(10) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `nxtcms_users`
---
-
-INSERT INTO `nxtcms_users` (`id`, `name`, `mail`, `username`, `password`, `lastlogin`, `level`) VALUES
-(1, NULL, 'test@test.no', 'Nils', 'pills', '2012-02-29 12:45:30', 2);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE IF NOT EXISTS `ffueater` (
+ `id` int(11) NOT NULL auto_increment,
+ `page` varchar(1000) collate utf8_unicode_ci NOT NULL,
+ `data` longtext collate utf8_unicode_ci NOT NULL,
+ `site` tinyint(4) NOT NULL,
+ `content` longtext collate utf8_unicode_ci,
+ `clean` longtext collate utf8_unicode_ci,
+ `wp_url` varchar(100) collate utf8_unicode_ci default NULL,
+ `wp_slug` varchar(50) collate utf8_unicode_ci default NULL,
+ `wp_postid` int(11) NOT NULL default '0',
+ `wp_guid` varchar(100) collate utf8_unicode_ci default NULL,
+ PRIMARY KEY  (`id`),
+ KEY `site` (`site`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
