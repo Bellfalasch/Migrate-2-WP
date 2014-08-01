49d1421e090db6d994daef7f6a4c0f6e8cd343b2
Bellfalasch <bellfalasch@gmail.com>
2014-08-01 20:04:33 +0200

    typo

--- _admin/migrate-step5.php
+++ _admin/migrate-step5.php
@@ -24,7 +24,7 @@
 // ****************************************************************************	
 
 	$new_site = "";
-	$oldsite = "";
+	$oldurl = "";
 	
 	$result = db_getSite(array('id' => $PAGE_siteid));
 	if ( isset( $result ) ) {
