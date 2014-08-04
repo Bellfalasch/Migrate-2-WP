6278f6c84615607ab4039f5d835a92e4b2862caf
Bellfalasch <bellfalasch@gmail.com>
2014-08-04 21:14:38 +0200

    Improved comments, translated and reordered

--- _admin/migrate-step3.php
+++ _admin/migrate-step3.php
@@ -37,7 +37,7 @@
 				
 				// Start replacing old bad markup ... at the moment very manual work =/
 
-				// Dessa taggar ær før summons olika levels, men tidy førstør dem pga att avslutande font-taggen tas bort av mig innan tidy får bita i koden
+				// These are tags from some of my own private projects that needed to get improved markup, just delete of you don't need which I guess you don't.
 				$clean = str_replace('<B><FONT COLOR="Orange">*</FONT>****</B>',         '<span class="stars"><span class="lit">*</span>****</span>', $clean);
 				$clean = str_replace('<B><FONT COLOR="Orange">*</FONT></B><B>****</B>',  '<span class="stars"><span class="lit">*</span>****</span>', $clean);
 				$clean = str_replace('<B><FONT COLOR="Orange">**</FONT>***</B>',         '<span class="stars"><span class="lit">**</span>***</span>', $clean);
@@ -63,23 +63,12 @@
 				$clean = str_replace('<B><FONT COLOR="Orange">**</FONT></B>', '<span class="stars"><span class="lit">**</span></span>', $clean);
 				$clean = str_replace('<B><FONT COLOR="Orange">*</FONT>*</B>', '<span class="stars"><span class="lit">*</span>*</span>', $clean);
 
-				// Old footer copyright notice from FF8-site
+				// Old footer copyright notice from an old site of mine, remove if you like
 				$clean = str_replace('<BR><CENTER><IMG SRC="../hr.jpg" WIDTH="430" HEIGHT="2"><FONT size="2" FACE="Arial" COLOR="#bbbbbb"><SMALL><BR>', '', $clean);
-				// $clean = str_replace('Site graphics, layout, text and parts of this site is ©opyright to <FONT COLOR="white">&lt;=- The Final Fantasy VIII Universe -=&gt;</FONT><BR>', '', $clean);
 				$clean = str_replace('Site graphics, layout, text and parts of this site is &copy;opyright to <FONT COLOR="white">&lt;=- The Final Fantasy VIII Universe -=&gt;</FONT><BR>', '', $clean);
 				$clean = str_replace('2000. Unauthorized reproduction or use of content on this site is prohibited. Squaresoft ® and<BR>', '', $clean);
 				$clean = str_replace('Final Fantasy, are registered trademarks of Square Co, Ltd.', '', $clean);
 
-				// $clean = str_replace('<FONT COLOR=red>', '<span class="color-red">', $clean);
-				// $clean = str_replace('<FONT COLOR=yellow>', '<span class="color-yellow">', $clean);
-//				$clean = str_replace('<FONT SIZE=2>', '', $clean);
-//				$clean = str_replace('<FONT COLOR=white SIZE=2>', '', $clean);
-//				$clean = str_replace('<FONT COLOR="#A0B1D2" SIZE="2" FACE="Arial">', '', $clean);
-//				$clean = str_replace('<FONT COLOR="#A0B1D2" SIZE="2">', '', $clean);
-				$clean = str_replace(' VALIGN="top"', '', $clean);
-				$clean = str_replace(' ALIGN="left"', '', $clean);
-				$clean = str_replace(' ALIGN="right"', '', $clean);
-				$clean = str_replace(' ALIGN="center"', '', $clean);
 				// Can't remove trailing font-tag or Tidy in next step will go nuts
 				//$clean = str_replace('</FONT>', '</span>', $clean);
 
@@ -91,16 +80,23 @@
 					$clean = preg_replace( '/<!--(.*)-->/Uis', '', $clean );
 				}
 
-				$clean = str_replace('<HR WIDTH="750" COLOR="black" NOSHADE>', '<hr />', $clean);
+				// Some markup we need to delete
 				$clean = str_replace('<CENTER><A HREF=#Upp><B>Upp</B></A></CENTER>', '', $clean);
 				$clean = str_replace('<CENTER><IMG SRC="hr.jpg" WIDTH="436" HEIGHT="2"><BR><BR>', '', $clean);
 				$clean = str_replace('<CENTER><FONT STYLE="font-size:10pt">', '', $clean);
-				$clean = str_replace('WIDTH="15" LENGTH="15"', 'width="15" height="15"', $clean);
 				$clean = str_replace('<td WIDTH="6"><FONT COLOR="black">.</font></td>', '', $clean);
 				$clean = str_replace('<TD NAME="space2" WIDTH=3><IMG SRC="trans.gif" WIDTH=3 HEIGHT=1></TD>', '', $clean);
 				$clean = str_replace('<BR><BR></TD></TR></TABLE>', '', $clean);
+				$clean = str_replace(' VALIGN="top"', '', $clean);
+				$clean = str_replace(' ALIGN="left"', '', $clean);
+				$clean = str_replace(' ALIGN="right"', '', $clean);
+				$clean = str_replace(' ALIGN="center"', '', $clean);
+				
+				// Some markup we can improve
+				$clean = str_replace('<HR WIDTH="750" COLOR="black" NOSHADE>', '<hr />', $clean);
+				$clean = str_replace('WIDTH="15" LENGTH="15"', 'width="15" height="15"', $clean);
 
-				// Donät think any of these are actually needed, so remove them all
+				// Some markup we potentially could improve, but they were more or less used for "design" and should be removed
 				$clean = str_replace('<CENTER>', '', $clean);
 				$clean = str_replace('</CENTER>', '', $clean);
 				$clean = str_replace('<SMALL>', '', $clean);
@@ -113,6 +109,7 @@
 				$clean = str_replace('bobby@westberg.org', 'webmaster@ffuniverse.nu', $clean);
 				$clean = str_replace('Bobby Vestberg', 'Bobby Westberg', $clean);
 
+				// Generate a view with original versus washed code
 				echo "<div class=\"spalt\"><strong>Original code:</strong>";
 				echo "<pre>" . htmlentities( $content, ENT_COMPAT, 'UTF-8', false ) . "</pre>";
 				echo "</div>";
@@ -120,11 +117,11 @@
 				echo "<div class=\"spalt\"><strong>Wash:</strong>";
 				echo "<pre class=\"clean\">" . htmlentities( $clean, ENT_COMPAT, 'UTF-8', false ) . "</pre>";
 
+				// Only save is the "Run"-button is pressed, skip if we're running a Test
 				if (formGet("save_wash") == "Run wash") {
 
 					echo "<p><strong>Result:</strong> <span class=\"label label-success\">Saved</span></p>";
 
-					// Pusha strippad data tillbaks in i databasen så kan vi køra en cleaner v2 på den strippade koden =)
 					db_MAIN("UPDATE migrate_content SET wash = '" . $mysqli->real_escape_string($clean) . "' WHERE id = " . $row->id . " LIMIT 1");
 
 					db_updateStepValue( array(
